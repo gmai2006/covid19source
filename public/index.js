806c394eb984faaf9c9a54f657bbb93bda98b899
Paul mai <gmai2006@gmail.com>
2023-10-30 23:16:51 -0700

    add select US states

---
+++ public/index.js
@@ -0,0 +1,10 @@
+import {
+  Breadcrumbs,
+} from './Base';
+import Charts from './Charts';
+
+export {
+  Charts,
+  Breadcrumbs,
+};
+
--- src/views/Growth.js
+++ src/views/Growth.js
@@ -1,8 +1,72 @@
 import React, { useState} from 'react';
-import { Card, CardBody, CardHeader } from 'reactstrap';
+import { Card, CardBody, CardHeader, FormGroup, Input, Label } from 'reactstrap';
 import {Col, Row } from 'reactstrap';
 
+const stateNames = `Alaska
+Alabama
+Arkansas
+American Samoa
+Arizona
+California
+Colorado
+Connecticut
+Delaware
+Florida
+Georgia
+Guam
+Hawaii
+Iowa
+Idaho
+Illinois
+Indiana
+Kansas
+Kentucky
+Louisiana
+Massachusetts
+Maryland
+Maine
+Michigan
+Minnesota
+Missouri
+Mississippi
+Montana
+North Carolina
+North Dakota
+Nebraska
+New Hampshire
+New Jersey
+New Mexico
+Nevada
+New York
+Ohio
+Oklahoma
+Oregon
+Pennsylvania
+Puerto Rico
+Rhode Island
+South Carolina
+South Dakota
+Tennessee
+Texas
+Utah
+Virginia
+Virgin Islands
+Vermont
+Washington
+Wisconsin
+West Virginia
+Wyoming`;
+
+let defaultChart = `./predicted_confirmed_growth_rate.png`;
+const states = stateNames.split(`\n`);
+console.log(states);
+const update = (event) => {
+  console.log(event);
+}
+
 function Growth() {
+  const [image, setImage] = useState('./predicted_confirmed_growth_rate.png');
+  console.log(image);
   return (
     <div className="animated fadeIn">
       <Row>
@@ -17,7 +81,27 @@ function Growth() {
               </div>
             </CardHeader>
             <CardBody>
-              <img src={ require('./predicted_confirmed_growth_rate.png') } />  
+            <FormGroup>
+              <Label for="exampleSelect">
+                Select
+              </Label>
+              <Input
+                id="states"
+                name="select"
+                type="select"
+                onClick={
+                  event => setImage(event.target.value)
+              }
+              >
+                {states.map((state) => (
+                  <option key={state}>
+                    {state}_confirm_predicted_growth_rate.png
+                  </option>
+                ))}
+                
+              </Input>
+            </FormGroup>
+              <img src={ require(`./predicted_confirmed_growth_rate.png`) } />  
             </CardBody>
           </Card>
           
